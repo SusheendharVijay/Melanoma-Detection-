c1b2ce6237e0499fbfad6aeb5677409b079fe10a
Sushi <susheendharvj@gmail.com>
2020-06-17 13:59:48 +0000

    main.py and resize_image.py

---
+++ create_folds.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+import os 
+from sklearn import model_selection 
+
+if __name__ =="__main__":
+    input_path = "/home/sushi/code/kaggle/Melanoma-Detection-/input/"
+    df = pd.read_csv(f"{input_path}/train.csv")
+    df["kfold"] = -1 
+    df = df.sample(frac=1).reset_index(drop=True)
+    y = df.target.values
+    kf = model_selection.StratifiedKFold(n_splits=5)
+
+    for fold_,(train_idx,test_idx) in enumerate(kf.split(X=df,y=y)):
+        df.loc[test_idx,"kfold"] = fold_
+    
+    df.to_csv(f"{input_path}/train_folds.csv",index=False)
+    
+
+
+
+
